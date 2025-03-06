ddb365bf03be8ebe1e4777bcba740f952656fcaa
Sona Leitmanova <sona.leitmanova@gmail.com>
2025-03-06 14:56:44 +0100

    FE  add simple tooltip for showing color names

--- finch-trade-fe/src/components/Item.tsx
+++ finch-trade-fe/src/components/Item.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Tooltip from "./Tooltip";
 import { Color } from "../types";
 
 interface Props {
@@ -13,17 +14,21 @@ const Item: FC<Props> = ({ color, text, itemId, handleRemove }) => {
     <div className="flex justify-between p-4 border rounded-lg mb-2">
       <div className="flex gap-4">
         {color.id === 1 ? (
-          <div className="w-6 h-6 rounded-full object-cover border border-black bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500"></div>
+          <Tooltip text={color.color}>
+            <div className="w-6 h-6 rounded-full object-cover border border-black bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500"></div>
+          </Tooltip>
         ) : (
-          <div
-            className="w-6 h-6 rounded-full object-cover border border-black"
-            style={{
-              backgroundColor:
-                color.id === 1
-                  ? "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)"
-                  : color.color,
-            }}
-          />
+          <Tooltip text={color.color}>
+            <div
+              className="w-6 h-6 rounded-full object-cover border border-black"
+              style={{
+                backgroundColor:
+                  color.id === 1
+                    ? "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)"
+                    : color.color,
+              }}
+            />
+          </Tooltip>
         )}
         <p className={`text-${color}-500 w-auto`}>{text}</p>
       </div>
---
+++ finch-trade-fe/src/components/Tooltip.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  text: string;
+}
+
+const Tooltip: FC<Props> = ({ children, text }) => {
+  return (
+    <div className="relative group inline-block">
+      {children}
+
+      <div
+        className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2
+    w-max px-3 py-1 text-sm text-white bg-gray-800 rounded-lg shadow-lg
+    opacity-0 group-hover:opacity-100 transition-opacity duration-200
+    z-50 pointer-events-none whitespace-nowrap"
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
