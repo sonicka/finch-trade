0f78654efedb9be6ec941a81cd52eefc5519dc11
Sona Leitmanova <sona.leitmanova@gmail.com>
2026-03-27 14:33:37 +0100

    fix: FE - header styling

--- finch-trade-fe/src/components/layout/UserHeader.tsx
+++ finch-trade-fe/src/components/layout/UserHeader.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Card from "../ui/Card";
 import Button from "../ui/Button";
 import { useUserData } from "../../context/UserProvider";
 
 const UserHeader: FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { user, logout } = useUserData();
   const { birbName, username } = user || {};
 
@@ -11,21 +14,27 @@ const UserHeader: FC = () => {
   return (
     <div className="mx-2 mt-2">
       <Card>
-        <div className="w-full flex gap-4 justify-between items-center">
-          {/* // todo <Button label="About" onClick={() => null} /> */}
-          <div />
+        <div className="grid grid-cols-3 w-full items-center">
+          <div className="flex justify-start">
+            {/* <Button label="About" onClick={() => null} /> */}
+          </div>
           <div className="flex flex-col gap-2 items-center">
             <img
               src="/finch.png"
               alt={birbName}
-              className="w-14 h-14 rounded-full object-cover"
+              className="w-14 h-14 rounded-full object-cover border-2 border-mediumBeige"
             />
-            <span className="text-lg font-semibold text-center">
-              {`${birbName} & ${username}`}
-            </span>
+            <Button
+              label={`${birbName} & ${username}`}
+              className="text-lg font-semibold hover:underline cursor-pointer hover:text-medium-beige transition-colors"
+              onClick={() =>
+                location.pathname !== "/profile" && navigate("/profile")
+              }
+            />
           </div>
-          {/* // todo <Button label="User Details" onClick={() => null} /> */}
-          <Button label="Logout" onClick={logout} />
+          <div className="flex justify-end">
+            <Button label="Logout" onClick={logout} />
+          </div>
         </div>
       </Card>
     </div>
